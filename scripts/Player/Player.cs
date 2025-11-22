b6990ea6c982e2667f8355e9d4c3b52ade4df834
Jakob Stechow <jakob.stechow@pm.me>
2025-11-22 12:36:32 +0100

    fix: player movement

--- scripts/Player/Player.cs
+++ scripts/Player/Player.cs
@@ -4,6 +4,7 @@ public partial class Player : CharacterBody3D
 {
     Camera3D playerCamera;
     private int _speed = 15;
+    private float _jumpVelocity = 10.0f;
 
     public override void _Ready()
     {
@@ -29,16 +30,18 @@ public partial class Player : CharacterBody3D
         {
             localVelocity.Z += 1.0f;
         }
-        if (IsOnFloor() && Input.IsActionJustPressed("jump"))
-        {
-            localVelocity.Y += 1.0f;
-        }
 
         localVelocity = localVelocity.Normalized();
+
         Godot.Basis basis = playerCamera.GlobalBasis;
         Vector3 movement = basis.X * localVelocity.X + basis.Z * localVelocity.Z;
 
         Velocity = new Vector3(movement.X * _speed, Velocity.Y, movement.Z * _speed);
+
+        if (IsOnFloor() && Input.IsActionJustPressed("jump"))
+        {
+            Velocity = new Vector3(Velocity.X, _jumpVelocity, Velocity.Z);
+        }
     }
 
     public override void _PhysicsProcess(double delta)
@@ -52,7 +55,11 @@ public partial class Player : CharacterBody3D
     {
         if (!IsOnFloor())
         {
-            Position += new Vector3(0.0f, -5.0f * (float)delta, 0.0f);
+            Velocity += new Vector3(0.0f, -30.0f * (float)delta, 0.0f);
+        }
+        else if (Velocity.Y < 0)
+        {
+            Velocity = new Vector3(Velocity.X, 0f, Velocity.Z);
         }
     }
 }
